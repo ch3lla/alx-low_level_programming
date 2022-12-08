1eed89b45e28c0a5ee01c1a786f0e9f308afab5e
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-08 02:11:53 -0800

    programming in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
 
  * main - Entry point
@@ -12,12 +10,8 @@
 
  */
 
-int main(void)
-  
+int main(void)  
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
+  puts("\"Programming is like building a multilingual puzzle"); 
   return (0);
-  
 }
