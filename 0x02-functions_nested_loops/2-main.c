240ea0b4d648b24e9d32517293893d1e7b79ee07
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-13 15:03:48 -0800

    please work

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
-void print_alphabet_x10();
-
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+void print_alphabet_x10()
+{
+  int n, co;
+
+  co = 0;
+
+  while (co < 10)
+    {
+      for (n = 'a'; n <= 'z'; n++)
+	{
+	  _putchar(n);
+	}
+      co++;
+      _putchar('\n');
+    }
+}
+
 int main(void)
 {
   print_alphabet_x10();
