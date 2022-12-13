937b1b3e1cfc1bd19a2967bea44cc2f87e0a6a1d
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-13 15:31:42 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void times_table(void);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  times_table();
+  return (0);
+}
