9a3abc769a85863ccbb5f95829d9be20fa2a2a1b
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-13 14:08:52 -0800

    printing lowercase alphabets

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -4,6 +4,16 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void)
+{
+  int n;
+
+  for (n = 'a'; n <= 'z'; n++)
+    {
+      _putchar(n);
+    }
+  _putchar('\n');
+}
 int main(void)
 {
   print_alphabet();
