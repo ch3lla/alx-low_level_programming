a2245bb3542685de0782111747876549146f1033
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-13 16:24:00 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
-extern int _putchar(char c);
-
 /**
- * main - Check description
+ * main - Entry point
  * Description: It prints the word _putchar, followed by a new line.
  * Return: Always 0 (Success).
  */
 int main(void)
 {
-  char word[8] = "_putchar";
-  int i;
-
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(word[i]);
-    }
+  _putchar(' ');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
   _putchar('\n');
-
+  
   return (0);
 }
