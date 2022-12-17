e095303bdad0a787cfbf0aaa75bde0e098cb4c14
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-17 03:13:06 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 1 - 10
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 1; c <= 10; c++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
