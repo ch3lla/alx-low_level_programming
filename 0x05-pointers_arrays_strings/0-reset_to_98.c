10e45fa2981e0afe462d90b656db36815139d125
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-19 16:01:07 -0800

    task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
- * reset_to_98 -takes a pointer to an int parameter  and updates the values to 98
- *
+ * reset_to_98 -takes a pointer to an int parameter
+ *                and updates the values to 98
  * @n: int parameter
  *
  * Return: 0
