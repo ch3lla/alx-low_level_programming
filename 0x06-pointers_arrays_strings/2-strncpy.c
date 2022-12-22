7fb2d6315698db1be66fb3df561cdbd6e69bf0da
ch3lla <chukwumaemmanuella03@gmail.com>
2022-12-22 14:20:05 -0800

    bad line correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,12 +7,12 @@
  * @src: The source string
  * @n: The maximum number of bytes to be copied from src
  *
-
  * Return: A pointer t othe resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-nt index = 0; src_len = 0;
+int index = 0;
+int src_len = 0;
 
 while (src[index++])
 {
