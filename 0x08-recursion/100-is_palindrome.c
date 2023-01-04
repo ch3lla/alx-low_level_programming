da864efb3a45c198b749f5a3318ac8a734eaaf86
ch3lla <chukwumaemmanuella03@gmail.com>
2023-01-04 13:37:37 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  *is_palindrome - determine if string is palindrome
- *@s - string
- *Return - 0 if empty
+ *@s - pointer to string
+ *Return - recursion
  */
 int is_palindrome(char *s)
 {
-  
+int len = _strlen_recursion(s);
+return (pl(s, len - 1));
+}
